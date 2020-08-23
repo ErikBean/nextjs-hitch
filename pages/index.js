e8a804025c539db26361dc0fcd8002bfcd7a3581
Erik Bean <Erik.Bean@nike.com>
2020-08-23 13:51:08 -0700

    svg stuff

--- pages/index.js
+++ pages/index.js
@@ -8,27 +8,23 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      {/* <h1 className="title">Erik + Katie + Morgan</h1> */}
       <main className="main">
+        {/* <h1 className="title">Erik <br/>+<br/> Katie <br/>+<br/> Morgan</h1> */}
         <figure className="shout-out">
-          <div className="row" id="row-1">
-            <span className="img-text" id="1">We're</span>
-          </div>
-          <div className="row" id="row-2">
-            <span className="img-text" id="2">Gettin'</span>
-          </div>
-          <div className="row" id="row-3">
-            <span className="img-text" id="3">Married!</span>
-          </div>
+          <svg className="svg" viewBox="0 0 56 18">
+            <text className="text" fill="white" x="0" y="15">We're</text>
+          </svg>
+          <svg className="svg" viewBox="0 0 56 18">
+            <text className="text" fill="white" x="0" y="15">Gettin</text>
+          </svg>
+          <svg className="svg" viewBox="0 0 60 18">
+            <text className="text" fill="white" x="0" y="15">Married</text>
+          </svg>
         </figure>
       </main>
 
       <style jsx>{`
-        .title {
-          text-align: center;
-          font-family: sans-serif;
-          font-size: 200px;
-        }
+        @import url('https://fonts.googleapis.com/css?family=Bowlby+One+SC');
         .main {
           display: flex;
           justify-content: center;
@@ -37,41 +33,21 @@ export default function Home() {
           margin: 0;
           display: flex;
           flex-direction: column;
-          height: 979px;
-          width: 734px;
+          justify-content: space-between;
+          height: 133vw;
+          width: 100vw;
+          max-width: 737px;
+          max-height: 979px;
           background-image: linear-gradient(
             rgba(0, 0, 0, 0.4),
             rgba(0, 0, 0, 0.4)
           ), url('/ErikKatieMorgan.jpg');
           background-repeat: no-repeat;
-          background-size: auto;
+          background-size: contain;
           color: white;
           font-family: sans-serif;
           font-weight: bold;
-        }
-        .row {
-          display: flex;
-          align-items: center;
-          flex-grow: 1;
-          text-align: center;
-        }
-        #row-1 {
-          font-size: 90px;
-          align-self: flex-end;
-          transform: translateX(-100px);
-        }
-        #row-2 {
-          font-size: 90px;
-          align-self: center;
-          transform: translateX(-68px) rotate(-45deg);
-        }
-        #row-3 {
-          font-size: 120px;
-          align-self: flex-end;
-          transform: translate(-80px, 110px);
-        }
-        .img-text {
-
+          border-radius: 30px;
         }
       `}</style>
 
