1df529454eba4d13859f4821412dc832184a112f
Erik Bean <Erik.Bean@nike.com>
2020-08-23 12:19:57 -0700

    picture text stuff

--- pages/index.js
+++ pages/index.js
@@ -4,21 +4,74 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>K&E</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <h1 className="title">Katie + Erik</h1>
+      {/* <h1 className="title">Erik + Katie + Morgan</h1> */}
+      <main className="main">
         <figure className="shout-out">
-          <h2>We're Getting Married!</h2>
-          <img src="https://lh3.googleusercontent.com/Z-vZ58ZFoEhK8VyvhGhvDSOwfllHNtNF9AgE22eyzceIAaAy6RA2F8bd-udJK91cdvWpP1tHqEJrqLyTQMPksTck_8oiJaZOB2e4A78T2Sfdm9y-jnpuf6_1bacf8vBwT5JsMa9_r18" />
+          <div className="row" id="row-1">
+            <span className="img-text" id="1">We're</span>
+          </div>
+          <div className="row" id="row-2">
+            <span className="img-text" id="2">Gettin'</span>
+          </div>
+          <div className="row" id="row-3">
+            <span className="img-text" id="3">Married!</span>
+          </div>
         </figure>
       </main>
 
       <style jsx>{`
         .title {
           text-align: center;
+          font-family: sans-serif;
+          font-size: 200px;
+        }
+        .main {
+          display: flex;
+          justify-content: center;
+        }
+        .shout-out {
+          margin: 0;
+          display: flex;
+          flex-direction: column;
+          height: 979px;
+          width: 734px;
+          background-image: linear-gradient(
+            rgba(0, 0, 0, 0.4),
+            rgba(0, 0, 0, 0.4)
+          ), url('/ErikKatieMorgan.jpg');
+          background-repeat: no-repeat;
+          background-size: auto;
+          color: white;
+          font-family: sans-serif;
+          font-weight: bold;
+        }
+        .row {
+          display: flex;
+          align-items: center;
+          flex-grow: 1;
+          text-align: center;
+        }
+        #row-1 {
+          font-size: 90px;
+          align-self: flex-end;
+          transform: translateX(-100px);
+        }
+        #row-2 {
+          font-size: 90px;
+          align-self: center;
+          transform: translateX(-68px) rotate(-45deg);
+        }
+        #row-3 {
+          font-size: 120px;
+          align-self: flex-end;
+          transform: translate(-80px, 110px);
+        }
+        .img-text {
+
         }
       `}</style>
 
